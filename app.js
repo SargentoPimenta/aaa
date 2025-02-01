a47438073afaf7a76176ee7c6b085e4c741b996b
SargentoPimenta <marcusbrazao09@gmail.com>
2025-02-01 15:45:44 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um número entre 1 a 10';
 let listaDeNumeroSorteados = []
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -26,7 +26,7 @@ function exibirMensagemInicial(){
 }
 
 exibirTextoNaTela('h1','Jogo do Número secreto!');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 //Código omitido
 
 function vC() {
